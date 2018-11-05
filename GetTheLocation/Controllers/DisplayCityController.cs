7f3e33b61b11ad433942740e135293dc6d2d5599
RomainP <romain.perrusset@etu.esisar.grenoble-inp.fr>
2018-11-05 17:40:14 +0100

    Add website project

---
+++ GetTheLocation/Controllers/DisplayCityController.cs
@@ -0,0 +1,34 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetTheLocation.Controllers
+{
+    public class DisplayCityController : Controller
+    {
+        // GET: DisplayCity
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult> Search(string search)
+        {
+            City city = await LocationBLL.GetCityByName(search);
+
+            if (city != null)
+            {
+                return View("DisplayCity", city);
+            }
+            else
+            {
+                return View("CityNotFounded");
+            }
+        }
+    }
+}
---
+++ GetTheLocation/ViewModels/SearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GetTheLocation.ViewModels
+{
+    public class SearchViewModel
+    {
+        [Required(ErrorMessage="Please ")]
+        public string search { get; set; }
+    }
+
+    //public class CoordinatesViewModel
+    //{
+    //    public double Latitude;
+    //    public double Longitude;
+    //}
+}
